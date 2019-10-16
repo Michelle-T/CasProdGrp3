974c1647f91cc5f3aeda7933725538a655cec0f6
tsevilla14272 <55515847+tsevilla14272@users.noreply.github.com>
2019-10-16 13:33:58 -0400

    Level 2 added

---
+++ Eco_Aquatic_Unity/Assets/Level2 Assets/BallScript.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallScript : MonoBehaviour
+{
+    public float ballInitialVelocity = 600f;
+
+
+    private Rigidbody2D rb;
+    private bool ballInPlay;
+
+    void Awake()
+    {
+
+        rb = GetComponent<Rigidbody2D>();
+        ballInPlay = false;
+
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(1) && ballInPlay == false)
+        {
+            transform.parent = null;
+            ballInPlay = true;
+            rb.isKinematic = false;
+            rb.AddForce(new Vector3(ballInitialVelocity, ballInitialVelocity, 0));
+        }
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Trash")
+        {
+            Destroy(collision.gameObject);
+        }
+    }
+}
