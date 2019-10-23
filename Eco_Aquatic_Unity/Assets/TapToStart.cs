20b4a39fc926e1f0bd04e8efaeb716c24d89d9f0
Michelle Truong <42843557+Michelle-T@users.noreply.github.com>
2019-10-23 19:49:30 -0400

    Title And Menu Updated
    Title scene created

---
+++ Eco_Aquatic_Unity/Assets/TapToStart.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TapToStart : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.touchCount >= 1)
+        {
+            Debug.Log("SceneName to load: MainMenu");
+            SceneManager.LoadScene("MainMenu");
+        }
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            Debug.Log("SceneName to load: MainMenu");
+            SceneManager.LoadScene("MainMenu");
+        }
+    }
+}
