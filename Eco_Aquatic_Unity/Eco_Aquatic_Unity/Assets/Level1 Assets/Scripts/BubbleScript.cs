9d7ca07fe2179f8baad643458b3a4ba0dfeb2c7d
Michelle Truong <michelletruong@knights.ucf.edu>
2019-11-03 01:12:03 -0400

    Test to Merge

---
+++ Eco_Aquatic_Unity/Eco_Aquatic_Unity/Assets/Level1 Assets/Scripts/BubbleScript.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BubbleScript : MonoBehaviour
+{
+    Rigidbody2D rb;
+
+    public float force;
+
+    private AudioSource audioSource;
+    private SpriteRenderer rend;
+    private PolygonCollider2D poly;
+    private CircleCollider2D poly2;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        Vector3 directon = new Vector3(0, force, 0);
+        rb.AddForce(directon, ForceMode2D.Impulse);
+    }
+       void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.tag == "Trash")
+        {
+            audioSource = GetComponent<AudioSource>();
+            audioSource.Play();
+            rend = GetComponent<SpriteRenderer>(); // gets sprite renderer
+
+            rend.enabled = false; // sets to false if hit.
+
+            poly2 = GetComponent<CircleCollider2D>();
+
+            poly2.enabled = false;
+            Destroy(gameObject, 3f);
+        }
+
+        if (other.gameObject.tag == "Fish")
+        {
+            audioSource = GetComponent<AudioSource>();
+            audioSource.Play();
+            rend = GetComponent<SpriteRenderer>(); // gets sprite renderer
+
+            rend.enabled = false; // sets to false if hit.
+
+            poly = GetComponent<PolygonCollider2D>();
+
+            poly.enabled = false;
+
+            Destroy(gameObject, 3f);
+        }
+    }
+    void OnBecameInvisible()
+    {
+        Destroy(gameObject);
+    }
+}
---
+++ Eco_Aquatic_Unity/Eco_Aquatic_Unity/Assets/MuteAudio.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MuteAudio : MonoBehaviour
+{
+    bool isMute;
+    public GameObject Mutet;
+    public GameObject Unmute;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Mute()
+    {
+        isMute = !isMute;
+        AudioListener.volume = isMute ? 0 : 1;
+    }
+
+    public void MuteText()
+    {
+        if (isMute == true)
+        {
+            Mutet.SetActive(false);
+            Unmute.SetActive(true);
+        }
+        else
+        {
+            Mutet.SetActive(true);
+            Unmute.SetActive(false);
+        }
+    }
+}
