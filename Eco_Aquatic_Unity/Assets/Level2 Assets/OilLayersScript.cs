d9eccb29c0855ab7f0802233d384e0426fa967f5
tsevilla14272 <55515847+tsevilla14272@users.noreply.github.com>
2019-11-03 12:29:37 -0500

    Added Oil Layers

---
+++ Eco_Aquatic_Unity/Assets/Level2 Assets/OilLayersScript.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OilLayersScript : MonoBehaviour
+{
+    public GameObject oilLayer1;
+    public GameObject oilLayer2;
+    public GameObject oilLayer3;
+    public GameObject oilLayer4;
+
+    void Update()
+    {
+        if (GameObject.FindGameObjectsWithTag("Trash").Length < 30)
+        {
+            Destroy(oilLayer1);
+        }
+
+        if (GameObject.FindGameObjectsWithTag("Trash").Length < 20)
+        {
+            Destroy(oilLayer2);
+        }
+
+        if (GameObject.FindGameObjectsWithTag("Trash").Length < 10)
+        {
+            Destroy(oilLayer3);
+        }
+
+        if (GameObject.FindGameObjectsWithTag("Trash").Length <= 0)
+        {
+            Destroy(oilLayer4);
+        }
+    }
+}
