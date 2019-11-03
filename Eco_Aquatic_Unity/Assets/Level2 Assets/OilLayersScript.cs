2b7520be357d8f31c45c0d106e7d7fdbd1365b50
tsevilla14272 <55515847+tsevilla14272@users.noreply.github.com>
2019-11-03 13:52:49 -0500

    Tried to add sparkle sound for layer destroy
    The sound plays too many times though

--- Eco_Aquatic_Unity/Assets/Level2 Assets/OilLayersScript.cs
+++ Eco_Aquatic_Unity/Assets/Level2 Assets/OilLayersScript.cs
@@ -9,26 +9,43 @@ public class OilLayersScript : MonoBehaviour
     public GameObject oilLayer3;
     public GameObject oilLayer4;
 
+    AudioSource audioData;
+    public AudioClip impact;
+
+    void Start()
+    {
+        audioData = GetComponent<AudioSource>();
+    }
+
     void Update()
     {
-        if (GameObject.FindGameObjectsWithTag("Trash").Length < 30)
+        if (GameObject.FindGameObjectsWithTag("Trash").Length == 30)
         {
             Destroy(oilLayer1);
+            playSound();
         }
 
-        if (GameObject.FindGameObjectsWithTag("Trash").Length < 20)
+        if (GameObject.FindGameObjectsWithTag("Trash").Length == 20)
         {
             Destroy(oilLayer2);
+            playSound();
         }
 
-        if (GameObject.FindGameObjectsWithTag("Trash").Length < 10)
+        if (GameObject.FindGameObjectsWithTag("Trash").Length == 10)
         {
             Destroy(oilLayer3);
+            playSound();
         }
 
-        if (GameObject.FindGameObjectsWithTag("Trash").Length <= 0)
+        if (GameObject.FindGameObjectsWithTag("Trash").Length == 0)
         {
             Destroy(oilLayer4);
+            playSound();
         }
+    }
+
+    void playSound()
+    {
+        audioData.PlayOneShot(impact, 0.7F);
     }
 }
