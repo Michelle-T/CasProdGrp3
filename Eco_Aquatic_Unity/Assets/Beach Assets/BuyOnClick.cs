af01fa2db9a529ceb709cca0e1489020ef5dc1d8
tsevilla14272 <55515847+tsevilla14272@users.noreply.github.com>
2019-11-25 18:09:50 -0500

    Push

---
+++ Eco_Aquatic_Unity/Assets/Beach Assets/BuyOnClick.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BuyOnClick : MonoBehaviour
+{
+    void OnMouseDown()
+    {
+        Debug.Log("Stop that, it tickles!");
+    }
+}
---
+++ Eco_Aquatic_Unity/Assets/Level1 Assets/Scripts/ShellScript.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShellScript : MonoBehaviour
+{
+    public Sprite sprite1;
+    public Sprite sprite2;
+
+    private SpriteRenderer spriteRenderer;
+
+    float elapsedTime = 0;
+
+    void Update()
+    {
+        elapsedTime += Time.deltaTime;
+        if(elapsedTime >= 5) 
+        {
+            ChangeTheDamnSprite;
+            elapsedTime = 0;
+        }
+    }
+
+    void ChangeTheDamnSprite()
+    {
+        if (spriteRenderer.sprite == sprite1) // if the spriteRenderer sprite = sprite1 then change to sprite2
+        {
+            spriteRenderer.sprite = sprite2;
+        }
+    }
+}
