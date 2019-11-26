1e8c625b54289059fe37754cfdd341757d61b243
tsevilla14272 <55515847+tsevilla14272@users.noreply.github.com>
2019-11-26 18:54:15 -0500

    Beach trash piles destroy on click of button

--- Eco_Aquatic_Unity/Assets/Beach Assets/BuyOnClick.cs
+++ Eco_Aquatic_Unity/Assets/Beach Assets/BuyOnClick.cs
@@ -4,8 +4,11 @@ using UnityEngine;
 
 public class BuyOnClick : MonoBehaviour
 {
+    public GameObject TrashPile;
+
     void OnMouseDown()
     {
         Debug.Log("Stop that, it tickles!");
+        Destroy(TrashPile.gameObject);
     }
 }
