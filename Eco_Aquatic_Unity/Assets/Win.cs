4dffc56655a2db44c9f666f0c4dfbcf5b68db0e8
Michelle Truong <michelletruong@knights.ucf.edu>
2019-11-26 22:16:42 -0500

    Beach Has Win State
    Beach scene has a win state

---
+++ Eco_Aquatic_Unity/Assets/Win.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Win : MonoBehaviour
+{
+    public GameObject[] winObjectsActiveness;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (GameObject.FindGameObjectsWithTag("Trash").Length <= 0)
+        {
+            foreach (GameObject g in winObjectsActiveness)
+            {
+                g.SetActive(true);
+            }
+        }
+    }
+}
