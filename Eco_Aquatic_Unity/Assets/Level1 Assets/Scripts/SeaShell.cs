7afe05d7759e7375f3de80ee790c3c9dd77cc329
Michelle Truong <42843557+Michelle-T@users.noreply.github.com>
2019-12-02 18:20:05 -0500

    SeaShell Flips
    Seashell flips :D

---
+++ Eco_Aquatic_Unity/Assets/Level1 Assets/Scripts/SeaShell.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SeaShell : MonoBehaviour
+{
+    public bool open;
+    public float countdownTimer;
+
+    public GameObject openshell;
+    public GameObject closeshell;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        countdownTimer -= Time.deltaTime;
+        if (countdownTimer < 0 && open == true)
+        {
+            open = false;
+            closeshell.SetActive(true);
+            openshell.SetActive(false);
+            countdownTimer = 3;
+        }
+        else if (countdownTimer < 0 && open == false)
+        {
+            open = true;
+            closeshell.SetActive(false);
+            openshell.SetActive(true);
+            countdownTimer = 3;
+        }
+    }
+}
